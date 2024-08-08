c45ed279c3fb3b50fd2f89162adfb9334e487b64
BCCheungGit <87406737+BCCheungGit@users.noreply.github.com>
2024-08-08 18:55:43 -0400

    added tilt to image

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,4 +1,24 @@
+"use client";
+
+import { Tilt } from "react-tilt";
+
+
+
+
 const Hero = () => {
+
+    const tiltOptions = {
+        reverse:        false,  // reverse the tilt direction
+        max:            30,     // max tilt rotation (degrees)
+        perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
+        scale:          0.95,    // 2 = 200%, 1.5 = 150%, etc..
+        speed:          1000,   // Speed of the enter/exit transition
+        transition:     true,   // Set a transition on enter/exit.
+        axis:           null,   // What axis should be disabled. Can be X or Y.
+        reset:          false,    // If the tilt effect has to be reset on exit.
+        easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+    }
+
   return (
     <section className="bg-background relative w-full h-screen mx-auto">
       <div className="sm:px-16 px-6 absolute inset-0 top-[120px] max-w-7xl mx-auto flex md:flex-row flex-col justify-between items-start gap-5">
@@ -10,9 +30,11 @@ const Hero = () => {
             A computer science student at NYU Shanghai
           </p>
         </div>
+        <Tilt options={tiltOptions}>
         <div className="md:w-[500px] w-fit">
           <img className="rounded-xl" src="/profilepic.jpg" />
         </div>
+        </Tilt>
       </div>
     </section>
   );
