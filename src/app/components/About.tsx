9824da204a4a4dfbbb33b6ff7403723a5cdd7300
BCCheungGit <bc3431@nyu.edu>
2024-08-09 16:58:27 -0400

    fixed error of unescaped entities

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -18,7 +18,7 @@ const About = () => {
 
             <div className="flex flex-col md:w-1/2 w-full">
             <h1 className="sm:text-[30px] text-[20px] text-white uppercase border-b-2 w-full">About Me</h1>
-            <p className="mt-10">I'm a sociable, quick-learning, curious thinker.</p>
+            <p className="mt-10">I&apos;m a sociable, quick-learning, curious thinker.</p>
             <p className="mt-5">I mainly work in Python and Typescript/Javascript, but I am working to expand my language skillset with C. I am especially proficient with full-stack web development, but am always open to learning new skills!</p>
             <p>In terms of non-tech related things, I enjoy swimming, piano, and ping pong!</p>
             <ScrollButton url="#work" text="Keep Going!" />
--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -25,7 +25,7 @@ const Hero = () => {
       <div className="sm:px-16 px-6 absolute inset-0 top-[120px] max-w-7xl mx-auto flex md:flex-row flex-col md:justify-between items-start gap-5">
         <div className="flex flex-col justify-center items-start mt-5">
           <h1 className="font-black text-white lg:text-[60px] sm:text-[40px] xs:text-[40px] text-[40px] lg:leading-[98px] md:mt-28 mt-0">
-            Hello, I'm Ben.
+            Hello, I&apos;m Ben.
           </h1>
           <p className="font-medium lg:text-[25px] sm:text-[20px] xs:text-[20px] text-[16px] lg:leading-[40px]">
             A computer science student at NYU Shanghai
